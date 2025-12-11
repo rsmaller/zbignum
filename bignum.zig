dfaf83cb3877677f0075a30c9742cae4f2cb5c32
ryan <ryjobobo@gmail.com>
2025-12-11 14:22:25 -0500

    Added back some stylistic changes to switch statements

--- bignum.zig
+++ bignum.zig
@@ -10,22 +10,15 @@ const stdout = &writer.interface;
 
 const bases = [_][]const u8 {
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
-    "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three",
-    "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine",
-    "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five",
-    "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one",
-    "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven",
-    "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three",
-    "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine",
-    "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five",
-    "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one",
-    "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six",
-    "seventy-seven", "seventy-eight", "seventy-nine", "eighty", "eighty-one",
-    "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six",
-    "eighty-seven", "eighty-eight", "eighty-nine", "ninety", "ninety-one",
-    "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six",
-    "ninety-seven", "ninety-eight", "ninety-nine"
+    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+    "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine",
+    "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine",
+    "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine",
+    "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine",
+    "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine",
+    "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine",
+    "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine",
+    "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine"
 };
 
 const single_digit_powers = [_][]const u8 {"thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion",
@@ -163,15 +156,9 @@ pub fn wordFromPower(num: usize, result: []u8) !usize {
 
 pub inline fn threeDigitStrToSmallInt(num : []const u8) !u10 {
     switch(num.len) {
-        0 => {
-            return error.SizeError;
-        },
-        1 => {
-            return (num[0] - '0');
-        },
-        2 => {
-            return (num[0] - '0') * 10 + (num[1] - '0');
-        },
+        0 => { return error.SizeError; },
+        1 => { return (num[0] - '0'); },
+        2 => { return (num[0] - '0') * 10 + (num[1] - '0'); },
         else => {
             return (@as(u10, num[0]) - '0') * 100 + (@as(u10, num[1]) - '0') * 10 + (@as(u10, num[2]) - '0');
         }
